9b7735ba4681363020b6a03f54ebb68c6970169a
Kolja Dummann <kolja.dummann@logv.ws>
2016-11-15 15:46:05 +0100

    start xvfb and set display variable

--- start.sh
+++ start.sh
@@ -8,4 +8,10 @@ export CONFIG_FILE=/buildAgent/conf/buildAgent.properties
 export PID_FILE=/run/tc-agent.pid
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
+export DISPLAY=:0.0
+
+XVFB=/usr/bin/Xvfb
+XVFBARGS="$DISPLAY -ac -screen 0 1024x768x16 +extension RANDR"
+PIDFILE=/var/xvfb_${DISPLAY:1}.pid
+/sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
 /buildAgent/bin/agent.sh run
